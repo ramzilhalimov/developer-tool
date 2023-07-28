1c44fab90fdbedcafebd86807ddaf2e067c1de1e
*твое <*>
2023-07-28 19:14:23 +0500

    homt-6 new

--- test/page-game-pages.test.ts
+++ test/page-game-pages.test.ts
@@ -19,17 +19,28 @@ it('should return 12 for medium difficulty', () => {
 
     expect(expected).toBe(result)
 })
+it('should return 18 for hard difficulty', () => {
+    const difficulty = 'hard'
+    const expected = 18
+
+    const result = 18
+
+    expect(expected).toBe(result)
+})
+
+it('should return 6 for unknown difficulty', () => {
+    const difficulty = 'unknown'
+    const expected = 6
+
+    const result = 6
+
+    expect(expected).toBe(result)
+})
 
 describe('shuffle()', () => {
     it('should shuffle the cards', () => {
         const array = ['1', '2', '3', '4']
         const shuffledArray = shuffle(array)
         expect(shuffledArray).toHaveLength(array.length)
-    })
-})
-describe('shuffle()', () => {
-    it('should shuffle the cards', () => {
-        const expected = ['array']
-        expect(['array']).toEqual(expect.arrayContaining(expected))
     })
 })
